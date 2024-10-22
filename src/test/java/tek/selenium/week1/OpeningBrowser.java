b43a62d88a742967d8989fdffa784813576c2143
Salma <selma.1390@gmail.com>
2024-10-21 20:25:01 -0400

    New changes!

---
+++ src/test/java/tek/selenium/week1/OpeningBrowser.java
@@ -0,0 +1,11 @@
+package tek.selenium.week1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpeningBrowser {
+    public static void main(String[] args) {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.facebook.com/");
+
+    }
+}
