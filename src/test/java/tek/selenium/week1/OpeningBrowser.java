e7bd5f46b4e0a86ad8e008bdd6aba06e0a894b02
Salma <selma.1390@gmail.com>
2024-10-21 20:30:38 -0400

    New changes!

--- src/test/java/tek/selenium/week1/OpeningBrowser.java
+++ src/test/java/tek/selenium/week1/OpeningBrowser.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpeningBrowser {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
+
         chromeDriver.get("https://www.facebook.com/");
+
+        chromeDriver.quit(); // close all the tabs
+        //chromeDriver.close(); // only one tab
 
     }
 }
