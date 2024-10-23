b9e96c3fa040c618393c4213b79b2e1f50e4cfcc
Salma <selma.1390@gmail.com>
2024-10-22 20:41:25 -0400

    Third Activity

---
+++ src/test/java/locators/SecondActivity.java
@@ -0,0 +1,24 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondActivity {
+    public static void main(String[] args) throws InterruptedException{
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+
+        By subTitleLocator = By.cssSelector(".top-nav__logo.active");
+        WebElement subTitleElement = driver.findElement(subTitleLocator);
+
+        String result =  subTitleElement.getText();
+        System.out.println(result);
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
