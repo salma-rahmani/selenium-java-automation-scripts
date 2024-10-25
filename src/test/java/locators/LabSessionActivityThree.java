9281d822420f8ddfba8d5e975545259459d543e2
Salma <selma.1390@gmail.com>
2024-10-24 21:18:30 -0400

    New Changes.

---
+++ src/test/java/locators/LabSessionActivityThree.java
@@ -0,0 +1,47 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LabSessionActivityThree {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://dev.retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+
+        By signInButtonLocator = By.linkText("Sign in");
+        WebElement signInElement = driver.findElement(signInButtonLocator);
+        signInElement.click();
+
+
+        By inputLocator = By.id("email");
+        WebElement inputElement = driver.findElement(inputLocator);
+        inputElement.sendKeys("selma.1390@gmail.com");
+        Thread.sleep(1000);
+
+        By passwordLocator = By.id("password");
+        WebElement passwordElement = driver.findElement(passwordLocator);
+        passwordElement.sendKeys("Password786");
+        Thread.sleep(1000);
+
+        By loginLocator = By.xpath("/html/body/div[1]/div[1]/div[2]/button/a");
+        WebElement loginElement = driver.findElement(loginLocator);
+        signInElement.click();
+
+        Thread.sleep(1000);
+
+        By errorLocator = By.className("ant-alert-message");
+        WebElement errorElement = driver.findElement(errorLocator);
+        String text =  errorElement.getText();
+        System.out.println(text);
+
+        Thread.sleep(2000);
+
+        driver.quit();
+
+
+    }
+}
--- src/test/java/locators/LabSessionActivityTwo.java
+++ src/test/java/locators/LabSessionActivityTwo.java
@@ -3,38 +3,39 @@ package locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import java.util.List;
 
 public class LabSessionActivityTwo {
     public static void main(String[] args) throws InterruptedException {
-
-        ChromeDriver driver = new ChromeDriver();
+            EdgeDriver driver = new EdgeDriver();
         driver.get("https://dev.retail.tekschool-students.com/");
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+        Thread.sleep(10000);
 
-        By searchInputLocator = By.id("searchInput");
-        WebElement searchInputElement = driver.findElement(searchInputLocator);
-        searchInputElement.sendKeys("Apple");
-        Thread.sleep(1000);
-
-        By searchBtnLocator = By.xpath("/html/body/div[1]/div[1]/span[1]/span[1]/span[1]/button");
-        WebElement searchBtnElement = driver.findElement(searchBtnLocator);
-        searchBtnElement.click();
-
-        By allLocators = By.tagName("h4");
-        List<WebElement> searchElements = driver.findElements(allLocators);
-
-            int searchItemNumber = searchElements.size();
-
-            if (searchItemNumber == 3) {
-                    System.out.println("The number of searched items is equal to three");
-            } else {
-                    System.out.println("More than three items displayed");
-            }
-
-        Thread.sleep(3000);
+//        By searchInputLocator = By.id("searchInput");
+//        WebElement searchInputElement = driver.findElement(searchInputLocator);
+//        searchInputElement.sendKeys("Apple");
+//        Thread.sleep(1000);
+//
+//        By searchBtnLocator = By.xpath("/html/body/div[1]/div[1]/span[1]/span[1]/span[1]/button");
+//        WebElement searchBtnElement = driver.findElement(searchBtnLocator);
+//        searchBtnElement.click();
+//
+//        By allLocators = By.tagName("h4");
+//        List<WebElement> searchElements = driver.findElements(allLocators);
+//
+//            int searchItemNumber = searchElements.size();
+//            System.out.println(searchItemNumber);
+//
+//            if (searchItemNumber == 3) {
+//                    System.out.println("The number of searched items is equal to three");
+//            } else {
+//                    System.out.println("More than three items displayed");
+//            }
+//
+//        Thread.sleep(10000);
         driver.quit();
 
     }
