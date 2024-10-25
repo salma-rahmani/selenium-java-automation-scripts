a42985a9fe7ee7d7b76f7000ae01baf35ef7c016
Salma <selma.1390@gmail.com>
2024-10-24 20:13:55 -0400

    New Changes.

---
+++ src/test/java/locators/LabSessionActivityTwo.java
@@ -0,0 +1,42 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class LabSessionActivityTwo {
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://dev.retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        By searchInputLocator = By.id("searchInput");
+        WebElement searchInputElement = driver.findElement(searchInputLocator);
+        searchInputElement.sendKeys("Apple");
+        Thread.sleep(1000);
+
+        By searchBtnLocator = By.xpath("/html/body/div[1]/div[1]/span[1]/span[1]/span[1]/button");
+        WebElement searchBtnElement = driver.findElement(searchBtnLocator);
+        searchBtnElement.click();
+
+        By allLocators = By.tagName("h4");
+        List<WebElement> searchElements = driver.findElements(allLocators);
+
+        int searchItemNumber = 0;
+        for (WebElement number : searchElements) {
+            searchItemNumber++;
+        }
+
+        if (searchItemNumber == 3) {
+            System.out.println("The number of searchedItems is equal to three");
+        } else System.out.println("More then three items displayed");
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+}
