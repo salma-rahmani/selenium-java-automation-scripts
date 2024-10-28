2d461f1ecd145c8aec340831b3014fb664585165
Salma <selma.1390@gmail.com>
2024-10-28 19:22:34 -0400

    New Changes of week two.

--- src/test/java/locators/Activity1Week2.java
+++ src/test/java/locators/Activity1Week2.java
@@ -8,31 +8,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Activity1Week2 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+
         driver.get("https://retail.tekschool-students.com/");
         driver.manage().window().maximize();
-
         Thread.sleep(1000);
 
         By searchInputLocator = By.xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/input");
         WebElement searchInputElement = driver.findElement(searchInputLocator);
         searchInputElement.sendKeys("TV");
-
         Thread.sleep(1000);
 
-
-        By searchBtnLocator = By.className("/html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button");
+        By searchBtnLocator = By.xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button");
         WebElement searchBtnElement = driver.findElement(searchBtnLocator);
         searchBtnElement.click();
         Thread.sleep(1000);
 
         driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/img")).click();
         String title = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/h1")).getText();
-        System.out.println(title);
+        System.out.println( title);
         Thread.sleep(1000);
 
         driver.quit();
-
-
-
     }
 }
